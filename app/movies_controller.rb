2f5538c5302ce244b9bfe6a1691c54ff88757850
Jamal Rjoul <jamensky1991@gmail.com>
2020-05-12 15:44:04 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -59,14 +59,13 @@ def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
   Movie.last.update(title: 'Even Awesomer Flick')
   Movie.last
-  #binding.pry
+  binding.pry
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  Movie.update()
 end
 
 def can_update_multiple_items_at_once
